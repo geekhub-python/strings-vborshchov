9ef875fb6262509c5783a8c42a5229ff03ecf1d5
Victor Borshchov <victor.borshchov@gmail.com>
2016-10-23 22:29:00 +0300

    Add words counter program

---
+++ task2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+string = 'Lorem ipsum dolor sit amet'
+
+print("Given string is:\n\n%s" % string)
+print("\n")
+words = string.count(" ") + 1
+
+print("1. Words quanity is %s" % words)
